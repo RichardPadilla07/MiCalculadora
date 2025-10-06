d0cee44153985b55d8bbb477b1165c8ba5aafaa3
RichardPadilla07 <richard.padilla@epn.edu.ec>
2025-10-06 16:46:52 -0500

    veamos

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,13 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'MiCalculadora',
-  webDir: 'www'
+  webDir: 'www',
+  android: {
+    backgroundColor: '#FFFFFF',
+    allowMixedContent: true,
+    captureInput: true,
+    initialFocus: true
+  }
 };
 
 export default config;
