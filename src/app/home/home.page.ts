71aeb28f756801f608273ebcb81afedf571a7ace
RichardPadilla07 <richard.padilla@epn.edu.ec>
2025-10-07 16:44:45 -0500

    cambio

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -16,7 +16,7 @@ export class HomePage {
   result: number | null = null;
   operation: string = '';
 
-  calculate(op: 'sum' | 'sub' | 'mul' | 'div') {
+  calculate(op: 'sum' | 'sub' | 'mul' | 'div' | 'sen' | 'cos' | 'tan' | 'sqrt') {
     const a = Number(this.num1);
     const b = Number(this.num2);
 
@@ -45,6 +45,47 @@ export class HomePage {
       } else {
         this.result = a / b;
         this.operation = 'División';
+      }
+
+    } else if (op === 'sen') {
+      if (b === 0 || b === null) {
+        this.result = Math.sin(a);
+        this.operation = 'Seno';
+      } else {
+        this.operation = 'La función seno solo requiere un número';
+        this.result = null;
+        return;
+      }
+      
+
+    } else if (op === 'cos') {
+      if (b === 0 || b === null) {
+        this.result = Math.cos(a);
+        this.operation = 'Coseno';
+      } else {
+        this.operation = 'La función coseno solo requiere un número';
+        this.result = null;
+        return;
+      }
+
+    } else if (op === 'tan') {
+      if (b === 0 || b === null) {
+        this.result = Math.tan(a);
+        this.operation = 'Tangente';
+      } else {
+        this.operation = 'La función tangente solo requiere un número';
+        this.result = null;
+        return;
+      }
+
+    } else if (op === 'sqrt') {
+      if (b === 0 || b === null) {
+        this.result = Math.sqrt(a);
+        this.operation = 'Raíz cuadrada';
+      } else {
+        this.operation = 'La función raíz cuadrada solo requiere un número';
+        this.result = null;
+        return;
       }
 
     } else {
